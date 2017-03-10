e617d76dda530a1fedca5e65eb553d7a9b99e6b4
Sdist <Sdist@192.168.1.3>
2017-03-11 00:48:01 +0800

    Eto na! Tama ba?

---
+++ src/javaproject/BishopPiece.java
@@ -0,0 +1,33 @@
+package javaproject;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sdist
+ */
+public class BishopPiece {
+    
+    private String ewan;
+    
+    
+    public BishopPiece()
+    {
+        
+        
+    }
+
+    /**
+     * @return the ewan
+     */
+    public String getEwan() {
+        return ewan;
+    }
+    
+    
+    
+}
---
+++ src/javaproject/RookPiece.java
@@ -0,0 +1,23 @@
+package javaproject;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sdist
+ */
+public class RookPiece {
+    
+    
+    
+    public RookPiece()
+    {
+        
+        
+    }
+    
+}
