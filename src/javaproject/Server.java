6ff852b1e8d69387cf3f92d407dbcb545c2be6da
Sdist <Sdist@192.168.1.3>
2017-03-14 01:22:17 +0800

    Merge origin/master
    Conflicts:
    	src/javaproject/MainFrame.java

--- src/javaproject/Server.java
+++ src/javaproject/Server.java
@@ -1,40 +1,55 @@
 package javaproject;
 
+import java.io.*;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Server implements Runnable{
 
-private Socket socket;//SOCKET INSTANCE VARIABLE
+    ServerSocket serversocket;
+    BufferedReader br1, br2;
+    PrintWriter pr1;
+    Socket socket;
+    Thread t1, t2;
+    String in="",out="";
 
-public Server(Socket s)
-{
-    socket = s;//INSTANTIATE THE SOCKET
-}
+    public Server() {
+        try {
+            t1 = new Thread(this);
+            t2 = new Thread(this);
+            serversocket = new ServerSocket(5000);
+            System.out.println("Server is waiting. . . . ");
+            socket = serversocket.accept();
+            System.out.println("Client connected with Ip " +        socket.getInetAddress().getHostAddress());
+            t1.start();;
+            t2.start();
 
-    @Override
-    public void run() //(IMPLEMENTED FROM THE RUNNABLE INTERFACE)
-    {
-        try //HAVE TO HAVE THIS FOR THE in AND out VARIABLES
+        } 
+        catch (Exception e) 
         {
-            Scanner in = new Scanner(socket.getInputStream());//GET THE SOCKETS INPUT STREAM (THE STREAM THAT YOU WILL GET WHAT THEY TYPE FROM)
-            PrintWriter out = new PrintWriter(socket.getOutputStream());//GET THE SOCKETS OUTPUT STREAM (THE STREAM YOU WILL SEND INFORMATION TO THEM FROM)
             
-            while (true)//WHILE THE PROGRAM IS RUNNING
-            {      
-                if (in.hasNext())
-                {
-                    String input = in.nextLine();//IF THERE IS INPUT THEN MAKE A NEW VARIABLE input AND READ WHAT THEY TYPED
-                    System.out.append(socket.getLocalAddress().getHostName() + input + "\n");//PRINT IT OUT TO THE SCREEN
-                    out.println("You Said: " + input);//RESEND IT TO THE CLIENT
-                    out.flush();//FLUSH THE STREAM
-               }
-            }
         }
-        catch (Exception e)
-        {
-            e.printStackTrace();//MOST LIKELY THERE WONT BE AN ERROR BUT ITS GOOD TO CATCH
-        }  
+     }
+
+     public void run() {
+        try {
+            if (Thread.currentThread() == t1) {
+                do {
+                    br1 = new BufferedReader(new InputStreamReader(System.in));
+                    pr1 = new PrintWriter(socket.getOutputStream(), true);
+                    in = br1.readLine();
+                    pr1.println(in);
+                } while (!in.equals("END"));
+            } else {
+                do {
+                    br2 = new BufferedReader(new   InputStreamReader(socket.getInputStream()));
+                    out = br2.readLine();
+                    System.out.println("Client says : : : " + out);
+                } while (!out.equals("END"));
+            }
+        } catch (Exception e) {
+        }
     }
 }
